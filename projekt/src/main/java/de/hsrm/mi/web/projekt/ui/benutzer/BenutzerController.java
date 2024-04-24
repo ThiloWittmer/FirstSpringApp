05997571e35afedf3e00b88e024d0f9e2cd8935b
Thilo Wittmer <thilo.wittmer@student.hs-rm.de>
2024-04-24 15:46:52 +0200

    Formular als SessionAttribute, Controller mit postMapping erweitert

--- projekt/src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
+++ projekt/src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
@@ -3,16 +3,28 @@ package de.hsrm.mi.web.projekt.ui.benutzer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
+@SessionAttributes(names = {"formular"})
 @RequestMapping("/benutzer")
 public class BenutzerController {
     
     @GetMapping("/{benutzerNr}")
-    public String benutzerProfil(@PathVariable("benutzerNr") int benNr, Model m) {
+    public String benutzerProfil(@PathVariable("benutzerNr") long benNr, Model m) {
         m.addAttribute("ueberschrift", ("Benutzerprofil " + benNr + " bearbeiten"));
+        m.addAttribute("formular", new BenutzerFormular());
+        return "benutzerbearbeiten";
+    }
+
+    @PostMapping("{benNr}")
+    public String postForm(@ModelAttribute("formular") BenutzerFormular form, Model m) {
+        String n = form.getName();
         return "benutzerbearbeiten";
     }
 }
---
+++ projekt/src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerFormular.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.web.projekt.ui.benutzer;
+
+public class BenutzerFormular {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
