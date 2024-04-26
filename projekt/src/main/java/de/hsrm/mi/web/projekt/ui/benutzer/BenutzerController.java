6759395f3ac144b19a059ef30b1371a27567cc87
Aaron Stier <aaron.stier@student.hs-rm.de>
2024-04-26 13:36:19 +0200

    Aufgabe 4: Formularfelder im html angepasst und als Sessionattribut hinzugefügt. Sessionattribute zum laufen gebracht und getestet.

--- projekt/src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
+++ projekt/src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
@@ -7,24 +7,30 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
-@SessionAttributes(names = {"formular"})
 @RequestMapping("/benutzer")
+@SessionAttributes("formular")
 public class BenutzerController {
-    
-    @GetMapping("/{benutzerNr}")
-    public String benutzerProfil(@PathVariable("benutzerNr") long benNr, Model m) {
-        m.addAttribute("ueberschrift", ("Benutzerprofil " + benNr + " bearbeiten"));
+
+    @ModelAttribute("formular")
+    public void creatForm(Model m){
         m.addAttribute("formular", new BenutzerFormular());
-        return "benutzerbearbeiten";
     }
 
+    @GetMapping("/{benutzerNr}")
+    public String benutzerProfil(@PathVariable("benutzerNr") long benNr, Model m, @ModelAttribute("formular") BenutzerFormular form) {
+        m.addAttribute("ueberschrift", ("Benutzerprofil " + benNr + " bearbeiten"));
+    return "benutzerbearbeiten";
+    }
+    
     @PostMapping("{benNr}")
     public String postForm(@ModelAttribute("formular") BenutzerFormular form, Model m) {
-        String n = form.getName();
-        return "benutzerbearbeiten";
+    return "benutzerbearbeiten";
     }
+    
 }
--- projekt/src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerFormular.java
+++ projekt/src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerFormular.java
@@ -1,7 +1,24 @@
 package de.hsrm.mi.web.projekt.ui.benutzer;
 
+import java.time.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 public class BenutzerFormular {
     private String name;
+    private String mail;
+    private String password;
+    @DateTimeFormat (iso = ISO.DATE)
+    private LocalDate bday;
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 
     public String getName(){
         return name;
@@ -10,4 +27,24 @@ public class BenutzerFormular {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBday() {
+        return bday;
+    }
+
+    public void setBday(LocalDate bday) {
+        this.bday = bday;
+    }
+
+    
+
+
 }
