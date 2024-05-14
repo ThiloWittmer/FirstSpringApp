8e987f8b7d147944c57da2f0b662fc83173a2d0f
Thilo Wittmer <thilo.wittmer@student.hs-rm.de>
2024-05-14 22:27:36 +0200

    anpassung python-skript: nachgucken, ob im richtigen directory ausgeführt wird

--- projekt/uebersetzungen-to-properties.py
+++ projekt/uebersetzungen-to-properties.py
@@ -1,13 +1,26 @@
 #!/usr/bin python3
+import os
 
-with open("uebersetzungen.csv", "r") as f:
+pathData = "uebersetzungen.csv"
+pathWrite = "./src/main/resources/messages"
+
+if 'projekt' in os.listdir():
+    pathData = "./projekt/uebersetzungen.csv"
+    pathWrite = "./projekt/src/main/resources/messages"
+
+with open(pathData, "r") as f:
     lines = f.readlines()
     langs = lines[0]
     langs = langs.split(";")[1:]
+    defaultMessProp = open(pathWrite + ".properties", "w")
     for i in range(len(langs)):
-        msg_prop = open("./src/main/resources/messages_" + langs[i].strip('\n') + ".properties", "x")
+        msg_prop = open(pathWrite + "_" + langs[i].strip('\n') + ".properties", "w")
         for line in lines[1:]:
             if  not line.startswith('#') and not line == "\n":
-                lineLst = line.split(";")
-                prop = lineLst[0] + "=" + lineLst[i+1].strip("\n") +"\n"
+                line = line.split(";")
+                prop = line[0] + "=" + line[i+1].strip("\n") +"\n"
                 msg_prop.write(prop)
+                if i == 0:
+                    defaultMessProp.write(prop)
+        msg_prop.close()
+        defaultMessProp.close()
