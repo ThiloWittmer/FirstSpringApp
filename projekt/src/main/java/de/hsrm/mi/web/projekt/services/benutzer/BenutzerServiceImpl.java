97b264979713abac4a4ada452f08e186f2b73945
Aaron Stier <aaron.stier@student.hs-rm.de>
2024-05-17 09:42:58 +0200

    Looging für den Benutzerservice hinzugefügt.

--- projekt/src/main/java/de/hsrm/mi/web/projekt/services/benutzer/BenutzerServiceImpl.java
+++ projekt/src/main/java/de/hsrm/mi/web/projekt/services/benutzer/BenutzerServiceImpl.java
@@ -3,13 +3,19 @@ package de.hsrm.mi.web.projekt.services.benutzer;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import de.hsrm.mi.web.projekt.entities.benutzer.Benutzer;
 import de.hsrm.mi.web.projekt.entities.benutzer.BenutzerRepository;
 
+@Service
 public class BenutzerServiceImpl implements BenutzerService{
+    private static final Logger logger = LoggerFactory.getLogger(BenutzerServiceImpl.class);
 
     private Benutzer benutzer;
     private BenutzerRepository repo;
@@ -17,12 +23,14 @@ public class BenutzerServiceImpl implements BenutzerService{
     @Autowired
     public BenutzerServiceImpl(Benutzer benutzer){
         this.benutzer = benutzer;
+        logger.debug("BenutzerServiceImpl instanziiert mit Benutzer: {}", benutzer);
     }
 
     @Override
     public List<Benutzer> holeAlleBenutzer() {
         // TODO Auto-generated method stub
         List<Benutzer> alleBenutzer = repo.findAll(Sort.by("name"));
+        logger.debug("Gefundene Benutzer: {}", alleBenutzer);
         return alleBenutzer;
 
     }
@@ -31,6 +39,7 @@ public class BenutzerServiceImpl implements BenutzerService{
     public Optional<Benutzer> holeBenutzerMitId(long id) {
         // TODO Auto-generated method stub
         Optional<Benutzer> benutzer = repo.findById(id);
+        benutzer.ifPresent(b -> logger.debug("Gefundener Benutzer: {}", b));
         return benutzer;
     }
 
